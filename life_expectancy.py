e1363e58b8e0bd013210393b5d419b4350405b23
mohammadmohsen33 <76225642+mohammadmohsen33@users.noreply.github.com>
2022-02-20 22:56:30 +0200

    Update life_expectancy.py

--- life_expectancy.py
+++ life_expectancy.py
@@ -1,8 +1,8 @@
 # importing libraries
-import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import streamlit as st
 import plotly.graph_objects as go
 
 # Function for making spaces
