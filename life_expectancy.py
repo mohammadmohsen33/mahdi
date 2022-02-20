920307202e58bfc95cdfbb86eb0f7148df604eff
mohammadmohsen33 <76225642+mohammadmohsen33@users.noreply.github.com>
2022-02-20 23:02:08 +0200

    Update life_expectancy.py

--- life_expectancy.py
+++ life_expectancy.py
@@ -1,6 +1,7 @@
 # importing libraries
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import plotly.express as px
 import streamlit as st
 import plotly.graph_objects as go
